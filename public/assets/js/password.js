b74b4b00f51003b8ddfbc093dbb8b159302f1146
Maxence Torchin <maxence.torchin@etu.univ-amu.fr>
2025-10-04 17:42:10 +0200

    Intégration du code des views respectives afin de ne pas avoir de JavaScript dans les views

--- public/assets/js/password.js
+++ public/assets/js/password.js
@@ -22,3 +22,22 @@ document.getElementById("codeForm").addEventListener("submit", e => {
         window.location.href = "passwordView.php";
     }
 });
+
+(function () {
+    const digits = Array.from(document.querySelectorAll('.code-digit'));
+    const hidden = document.getElementById('code');
+    if (digits.length && hidden) {
+        digits.forEach((inp, idx) => {
+            inp.addEventListener('input', () => {
+                inp.value = inp.value.replace(/[^0-9]/g, '').slice(0,1);
+                if (inp.value && idx < digits.length - 1) digits[idx+1].focus();
+                hidden.value = digits.map(d => d.value || '').join('');
+            });
+            inp.addEventListener('keydown', (e) => {
+                if (e.key === 'Backspace' && !inp.value && idx > 0) {
+                    digits[idx-1].focus();
+                }
+            });
+        });
+    }
+})();

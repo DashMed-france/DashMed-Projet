0860ea9993621a9e506433b266d516c6b2b1e36c
PGP_667 <julesribbe@gmail.com>
2025-10-13 13:46:48 +0200

    Update dashboardView.php

--- app/views/dashboardView.php
+++ app/views/dashboardView.php
@@ -52,6 +52,8 @@ class dashboardView
             <link rel="stylesheet" href="assets/css/themes/light.css">
             <link rel="stylesheet" href="assets/css/style.css">
             <link rel="stylesheet" href="assets/css/dash.css">
+            <link rel="stylesheet" href="assets/css/components/sidebar.css">
+            <link rel="stylesheet" href="assets/css/components/searchbar.css">
             <link rel="icon" type="image/svg+xml" href="assets/img/logo.svg">
         </head>
         <body>
